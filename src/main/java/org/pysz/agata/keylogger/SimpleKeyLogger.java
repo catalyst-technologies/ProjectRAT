4194fdbf3b22a3e2a626034ed46d5a03e5fcaf61
Mark Clu Heramis <chumheramis@gmail.com>
2017-06-15 15:48:21 +0800

    added new windowless runnable

--- src/main/java/org/pysz/agata/keylogger/SimpleKeyLogger.java
+++ src/main/java/org/pysz/agata/keylogger/SimpleKeyLogger.java
@@ -1,7 +1,6 @@
 /*
  * 
  */
-
 package org.pysz.agata.keylogger;
 
 import javax.swing.JFrame;
@@ -19,42 +18,42 @@ public class SimpleKeyLogger {
 
     public static void main(String[] args) {
 
-	// Schedule a job for the event dispatch thread:
-	// creating and showing this application's GUI.
-	javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	    public void run() {
-		init();
-	    }
-
-	});
+        // some code
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // do stuff in this thread
+                init();
+            }
+        }).start();
     }
 
     /**
      * init application: create window and register the key listeners
      */
     private static void init() {
-	SimpleKeyLogger simpleKeyLogger = new SimpleKeyLogger();
-	simpleKeyLogger.createAndShowGUI();
-	simpleKeyLogger.initializeKeyListeners();
+        SimpleKeyLogger simpleKeyLogger = new SimpleKeyLogger();
+        simpleKeyLogger.createAndShowGUI();
+        simpleKeyLogger.initializeKeyListeners();
     }
 
     /**
      * Create the GUI and show it.
      */
     private void createAndShowGUI() {
-	// Create and set up the window.
-	JFrame frame = new JFrame("Key Logger");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Create and set up the window.
+        JFrame frame = new JFrame("Key Logger");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	// Add contents to the window.
-	loggerPanel = new LoggerPanel();
-	frame.add(loggerPanel);
+        // Add contents to the window.
+        loggerPanel = new LoggerPanel();
+        frame.add(loggerPanel);
 
-	// Display the window.
-	frame.pack();
-	frame.setVisible(true);
+        // Display the window.
+        frame.pack();
+        frame.setVisible(true);
 
-	loggerPanel.appendTextToPanel("--start logging--");
+        loggerPanel.appendTextToPanel("--start logging--");
 
     }
 
@@ -62,20 +61,22 @@ public class SimpleKeyLogger {
      * initialize listeners which will listen for key events
      */
     private void initializeKeyListeners() {
-	new GlobalKeyListener().addKeyListener(new KeyAdapter() {
-	    @Override
-	    public void keyPressed(KeyEvent event) {
+        new GlobalKeyListener().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent event) {
                 System.out.println(event.toString());
-		loggerPanel.appendTextToPanel(event.toString());
-	    }
-	    @Override
-	    public void keyReleased(KeyEvent event) {
+                loggerPanel.appendTextToPanel(event.toString());
+            }
+
+            @Override
+            public void keyReleased(KeyEvent event) {
                 System.out.println(event.toString());
-		loggerPanel.appendTextToPanel(event.toString());
-		if (event.getVirtualKeyCode() == KeyEvent.VK_ADD && event.isCtrlPressed())
-		    loggerPanel.appendTextToPanel("CTRL+ADD was just released (CTRL is still pressed)");
-	    }
-	});
+                loggerPanel.appendTextToPanel(event.toString());
+                if (event.getVirtualKeyCode() == KeyEvent.VK_ADD && event.isCtrlPressed()) {
+                    loggerPanel.appendTextToPanel("CTRL+ADD was just released (CTRL is still pressed)");
+                }
+            }
+        });
 
     }
 
